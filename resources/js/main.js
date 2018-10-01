73e786da75aa3a945e903d843c8e96e91d91d74d
Bezlonir <michael_bragg99@hotmail.com>
2018-10-01 14:24:45 -0600

    Complete initial coding.

---
+++ resources/js/main.js
@@ -0,0 +1,37 @@
+const tSub = document.getElementById('task-submit');
+const tCurrent = document.getElementById('task-input');
+const tList = document.getElementById('todo-out');
+
+const procrastinationList = [
+    'Watch Netflix',
+    'Play some video games',
+    'Tweak resume',
+    'Work on darts skills',
+    'Check Facebook',
+    'Read the news',
+    'Hone Wizarding skills',
+    'Read a book',
+    'Smell the flowers',
+    'Take a walk',
+    'Stare into the distance',
+    'Browse Reddit',
+    'Search for unicorns',
+    'Window shop on Amazon',
+    'Weigh your options',
+];
+
+tSub.addEventListener('click', addItem);
+tCurrent.addEventListener('keyup', function(e){
+    if (e.keyCode === 13) {
+        addItem();
+    };
+});
+
+function addItem(e){
+    if (!tCurrent) return;
+    let newLI = document.createElement('LI');
+    let todoIndex = Math.floor(Math.random() * procrastinationList.length);
+    newLI.innerText = procrastinationList[todoIndex];
+    tList.appendChild(newLI);
+    tCurrent.value = '';
+}
